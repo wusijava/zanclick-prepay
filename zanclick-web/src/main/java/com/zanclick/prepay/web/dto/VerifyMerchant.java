58572c2d968257f82e432289297353d6e4bac550
duchong <long5262@qq.com>
2019-09-25 19:27:08 +0800

    商户信息验证方法

---
+++ zanclick-web/src/main/java/com/zanclick/prepay/web/dto/VerifyMerchant.java
@@ -0,0 +1,27 @@
+package com.zanclick.prepay.web.dto;
+
+import com.zanclick.prepay.common.entity.Param;
+import lombok.Data;
+
+/**
+ * 预授权商户创建
+ *
+ * @author duchong
+ * @date 2019-7-8 15:49:20
+ */
+@Data
+public class VerifyMerchant extends Param {
+    /**
+     * 商户号
+     */
+    private String merchantNo;
+
+    private String merchantName;
+
+    private Integer registerStatus;
+
+    private String managerName;
+
+    private String managerMobile;
+
+}
